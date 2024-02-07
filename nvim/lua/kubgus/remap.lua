afc27543678732c38ba7f9d9d54aadf738923fab
kubgus <ja.gustafik@outlook.sk>
2024-02-07 16:51:28 +0100

    Add <leader>p

--- nvim/lua/kubgus/remap.lua
+++ nvim/lua/kubgus/remap.lua
@@ -8,6 +8,9 @@ vim.g.mapleader = " "
 -- Netrw easy access (project-overview)
 vim.keymap.set("n", "<leader>po", vim.cmd.Ex)
 
+-- greatest remap ever
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
 -- Visual move
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
