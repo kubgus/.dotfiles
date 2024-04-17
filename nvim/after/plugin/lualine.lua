7758bf62b0e267f3ba2fb580985867c4479072ef
kubgus <ja.gustafik@outlook.sk>
2024-04-17 17:31:46 +0200

    Optimize lualine

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -188,12 +188,12 @@ ins_right {
   color = { fg = colors.green, gui = 'bold' },
 }
 
-ins_right {
-  'fileformat',
-  fmt = string.upper,
-  icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
-  color = { fg = colors.green, gui = 'bold' },
-}
+-- ins_right {
+--  'fileformat',
+--  fmt = string.upper,
+--  icons_enabled = false, -- I think icons are cool but Eviline doesn't have them. sigh
+--  color = { fg = colors.green, gui = 'bold' },
+--}
 
 ins_right {
   'branch',
