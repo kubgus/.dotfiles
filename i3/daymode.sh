694d7632e5d9dcc25c02babb9c5e2ada2627a249
kubgus <ja.gustafik@outlook.sk>
2024-06-03 00:57:43 +0200

    Make nightmode a toggle

--- i3/daymode.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-xrandr --output $1 --brightness 1
-xrandr --output $2 --brightness 1
-redshift -O 6500
--- i3/nightmode.sh
+++ i3/nightmode.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
-xrandr --output $1 --brightness 0.6
-xrandr --output $2 --brightness 0.6
-redshift -O 3000
+Current=$(xrandr --verbose | grep Brightness | awk '{print $2}')
+
+if [ "$Current" == "0.50" ]; then
+    xrandr --output $1 --brightness 1 --gamma 1.0:1.0:1.0
+    xrandr --output $2 --brightness 1 --gamma 1.0:1.0:1.0
+else
+    xrandr --output $1 --brightness 0.5 --gamma 1.0:0.88:0.76
+    xrandr --output $2 --brightness 0.5 --gamma 1.0:0.88:0.76
+fi
