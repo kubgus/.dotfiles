56300153b7cb5a92255eed3fcf88681d6ad05acf
kubgus <ja.gustafik@outlook.sk>
2024-11-04 20:47:27 +0100

    Change nightmode script

--- i3/nightmode.sh
+++ i3/nightmode.sh
@@ -2,10 +2,11 @@
 
 Current=$(xrandr --verbose | grep Gamma | awk '{print $2}' | head -1)
 
-if [ "$Current" == "1.0:1.0:1.0" ]; then
+                  # ⬇ dunno why this value works
+if [ "$Current" == "1.0:1.0:1.1" ]; then
     xrandr --output $1 --gamma 1.0:0.88:0.76
     xrandr --output $2 --gamma 1.0:0.88:0.76
 else
-    xrandr --output $1 --gamma 1.0:1.0:1.0
-    xrandr --output $2 --gamma 1.0:1.0:1.0
+    xrandr --output $1 --gamma 1.0:1.0:0.9
+    xrandr --output $2 --gamma 1.0:1.0:0.9
 fi
