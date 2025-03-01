a097763120a8500b646abd15f192ebb0a165a66c
kubgus <business@gustafik.com>
2025-03-01 01:42:09 +0100

    Actually add missing dot

--- nvim/lua/kubgus/remap.lua
+++ nvim/lua/kubgus/remap.lua
@@ -33,7 +33,7 @@ vim.keymap.set("n", "<leader>Y", "\"+Y")
 -- Search and replace
 vim.keymap.set("n", "<leader>sara", ":%s/") -- All
 vim.keymap.set("n", "<leader>sarr", ":+s/") -- Default
-vimkeymap.set("n", "<leader>sarn", ":s/") -- Normal
+vim.keymap.set("n", "<leader>sarn", ":s/") -- Normal
 
 -- C++ (almost illegal)
 vim.keymap.set("n", "<leader>cppm", "iint main() {<Enter>}<Esc>O") -- Main
